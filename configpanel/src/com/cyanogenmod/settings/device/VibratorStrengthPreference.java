4027e38cbc8caf680c907717e22e8cedf1ba21e7
HENDY <hendyaugustama@gmail.com>
2019-05-03 10:16:26 +0700

    Change cyanogenmod

--- configpanel/src/com/cyanogenmod/settings/device/VibratorStrengthPreference.java
+++ configpanel/src/com/cyanogenmod/settings/device/VibratorStrengthPreference.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package org.omnirom.device;
+package com.cyanogenmod.settings.device;
 
 import android.content.ContentResolver;
 import android.content.Context;
