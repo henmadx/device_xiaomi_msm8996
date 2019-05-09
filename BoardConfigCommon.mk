6a4e5677a74002567a54e2aa33ce2e13c78cb955
Hendy A <45126844+henmadx@users.noreply.github.com>
2019-05-09 20:04:03 +0700

    msm8996-common: Preopt again

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -108,13 +108,14 @@ DEVICE_SPECIFIC_CAMERA_PATH := $(VENDOR_PATH)/camera
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
-# Enable dex pre-opt to speed up initial boot
+# Enable dex pre-opt to speed up initial boo
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_DEBUG_INFO := false
+      USE_DEX2OAT_DEBUG := false
+      WITH_DEXPREOPT_PIC := true
       WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
   endif
 endif
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
