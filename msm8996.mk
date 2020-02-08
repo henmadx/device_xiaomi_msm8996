a3fe4bddc1b8be4881bab5b1376f5622711e0ec9
henmadx <hendyaugustama@gmail.com>
2020-02-08 07:02:05 +0700

    msm8996-common: kang health from marlin
    * Source : https://android.googlesource.com/device/google/marlin/+/refs/tags/android-10.0.0_r29/health/

--- msm8996.mk
+++ msm8996.mk
@@ -224,8 +224,7 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.0-service.capricorn
 
 # HIDL
 PRODUCT_PACKAGES += \
